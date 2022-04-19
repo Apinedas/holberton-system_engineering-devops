4bc663dba147a6b364a5c710e6d8b5726bd1451d
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-04-19 12:05:16 -0500

    fix: changed 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install puppet-lint version 2.5.0
 package { 'puppet-lint':
-  provider        =>  gem,
-  source          =>  'https://rubygems.org/gems/puppet-lint/versions/2.5.0'
+  ensure   => '2.5.0',
+  provider =>  gem,
+  source   =>  'https://rubygems.org/gems/puppet-lint/versions/2.5.0'
 }
